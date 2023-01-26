80a4ec855c9affffdb22cbfc4ebbdb9feac35c8c
Richard Garrison <anm8tr@users.noreply.github.com>
2023-01-26 15:03:53 -0700

    Initial Commit

---
+++ AddMyClient/AddMyClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddMyClientApp.swift
+//  AddMyClient
+//
+//  Created by Richard Garrison on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct AddMyClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
